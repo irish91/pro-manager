1015828b4d738ff700e575cf11033df8f63ea616
Tobias Hourst <tobias@strawberrydigital.com.au>
2012-04-11 02:04:43 +0200

    Added background

---
+++ src/Pmf/StaticBundle/Controller/ServerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pmf\StaticBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ *
+ * @author Tobias Hourst <tobiashourst@elcyone.com>
+ *
+ */
+class ServerController extends Controller
+{
+    
+    public function indexAction($page)
+    {	
+        // get template
+        $template = sprintf("PmfStaticBundle:Page:%s.html.twig", $page);
+        
+        // check if template exists
+        if(!$this->get('templating')->exists($template))
+        	throw new NotFoundHttpException("The specified page does not exist.");
+        
+        // render current view
+        return $this->render($template);
+    }
+}
+
