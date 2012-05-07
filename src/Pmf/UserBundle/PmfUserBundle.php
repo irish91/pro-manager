582ea9593407cfdcc4ed39dd031dadc385c878d4
Tobias Hourst <tobiashourst@elcyone.com>
2012-05-08 01:02:08 +0200

    PmfUserBundle now extends FOSUserBundle

--- src/Pmf/UserBundle/PmfUserBundle.php
+++ src/Pmf/UserBundle/PmfUserBundle.php
@@ -5,5 +5,12 @@ namespace Pmf\UserBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PmfUserBundle extends Bundle
-{
+{	
+	/**
+	 * - extends FOSUserBundle
+	 */
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
 }
