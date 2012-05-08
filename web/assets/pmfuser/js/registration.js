79c0fdaed916785e61203b5aa2df5095b8de84c6
Tobias Hourst <tobiashourst@elcyone.com>
2012-05-08 04:24:57 +0200

    Added register box

---
+++ web/assets/pmfuser/js/registration.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('input#register').click(function(){
+		
+		
+		return false;
+	});
+	
+	$('input#register').fancybox({ 'autoDimensions': true, 'padding': -5, 'margin': 0 });
+	
+});
