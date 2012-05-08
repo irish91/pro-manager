9c18215de937c44080ee7bffc626221dcb35cf06
Tobias Hourst <tobiashourst@elcyone.com>
2012-05-08 02:05:27 +0200

    Overrided registerAction
    no changes has been made yet. Just prepared it for the AJAX call

--- src/Pmf/UserBundle/Controller/RegistrationController.php
+++ src/Pmf/UserBundle/Controller/RegistrationController.php
@@ -6,13 +6,49 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
 /**
  *
  * @author Tobias Hourst <tobiashourst@elcyone.com>
+ * 
+ * - extends FOSUserBundle RegistrationController
  */
-class RegistrationController extends Controller
+class RegistrationController extends BaseController
 {
-    
+    /**
+     * Ovverides registerAction
+     */
+	public function registerAction()
+	{
+		$form = $this->container->get('fos_user.registration.form');
+		$formHandler = $this->container->get('fos_user.registration.form.handler');
+		$confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+	
+		$process = $formHandler->process($confirmationEnabled);
+		if ($process) {
+			$user = $form->getData();
+	
+			if ($confirmationEnabled) {
+				$this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+				$route = 'fos_user_registration_check_email';
+			} else {
+				$this->authenticateUser($user);
+				$route = 'fos_user_registration_confirmed';
+			}
+	
+			$this->setFlash('fos_user_success', 'registration.flash.user_created');
+			$url = $this->container->get('router')->generate($route);
+	
+			return new RedirectResponse($url);
+		}
+	
+		return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+				'form' => $form->createView(),
+				'theme' => $this->container->getParameter('fos_user.template.theme'),
+		));
+	}
+	
 	public function createTeamAction(){
 		
 		return $this->render('PmfUserBundle:Registration:create-team.html.twig');
