5e2b7bc1128f3d1122877d4b0abf621a07c1b203
Tobias Hourst <tobiashourst@elcyone.com>
2012-06-12 18:52:40 +0200

    Generated PmfGameInterfaceBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
         	new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         
             new Pmf\UserBundle\PmfUserBundle(),
+            new Pmf\GameInterfaceBundle\PmfGameInterfaceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Pmf/GameInterfaceBundle/PmfGameInterfaceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pmf\GameInterfaceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PmfGameInterfaceBundle extends Bundle
+{
+}
